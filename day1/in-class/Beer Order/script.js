b71ae6d39c114f470688ddaaa2651079fcbb0eeb
sRanay <jsong2506@gmail.com>
2022-05-23 20:47:27 +0800

    Added Beer Order

--- day1/in-class/Beer Order/script.js
+++ day1/in-class/Beer Order/script.js
@@ -2,6 +2,9 @@ var main = function (input) {
   // Beer Order
   // Input: Average Number of Customers per day
   // Output: Amount of Half-barrel-size kegs the bar needs per quarter
-  
+  var pintsPerDay = input * 2;
+  var kegsPerDay = pintsPerDay / 124;
+  var kegsPerQuarter = kegsPerDay * (365 / 4);
+  myOutputValue = kegsPerQuarter + " kegs will be needed per quarter.";
   return myOutputValue;
 };

1f9a1ce5d28145d056f4c065c722e86d4785fc09
sRanay <jsong2506@gmail.com>
2022-05-30 00:15:56 +0800

    Added reversed mode

--- day3/in-class/Scissors Paper Stone P1/script.js
+++ day3/in-class/Scissors Paper Stone P1/script.js
@@ -1,8 +1,12 @@
+// Global Variable
+var reversedMode = false;
+var normalMode = true;
+
 // Function checkUserInput
 // Input: User Input
 // Output: Return False if it does not have the correct input
 var checkUserInput = function (userInput) {
-  if (userInput === "scissors" || userInput === "paper" || userInput === "stone") {
+  if (userInput === "scissors" || userInput === "paper" || userInput === "stone" || userInput === "reversed" || userInput === "normal") {
     return true;
   } else {
     return false;
@@ -35,7 +39,17 @@ var convertToIcon = function (choice) {
   } else {
     return "🪨";
   }
-}
+};
+
+var winOrLose = function (user1,user2) {
+  if(
+    user1 === "scissors" && user2 === "paper" ||
+    user1 === "paper" && user2 === "stone" ||
+    user1 === "stone" && user2 === "scissors") {
+      return true;
+    }
+    return false;
+};
 
 var main = function (input) {
   var myOutputValue = 'you lose';
@@ -44,22 +58,36 @@ var main = function (input) {
     myOutputValue = "Please enter a valid input (scissors, paper or stone)";
     return myOutputValue
   }
+  if (input === "reversed") {
+    myOutputValue = "Welcome to Reversed Mode."
+    normalMode = false;
+    reversedMode = true;
+    return myOutputValue;
+  }
+  if (input === "normal") {
+    myOutputValue = "Welcome to Normal Mode."
+    normalMode = true;
+    reversedMode = false;
+    return myOutputValue;
+  }
   var computerChoice = generateSPS();
   var computerIcon = convertToIcon(computerChoice);
   var userIcon = convertToIcon(input);
-  console.log("Computer: ",computerChoice);
-  console.log("User: ",input);
+  if (normalMode === true) {
+    var userWon = winOrLose(input, computerChoice);
+    var computerWon = winOrLose(computerChoice, input);
+  } else {
+    var userWon = winOrLose(computerChoice, input);
+    var computerWon = winOrLose(input, computerChoice);
+  }
+  console.log("Computer: ", computerChoice);
+  console.log("User: ", input);
+  console.log("Reversed Mode: ", reversedMode);
+  console.log("Normal Mode: ", normalMode);
   // Situation where user wins
-  if(
-      input === "scissors" && computerChoice === "paper" ||
-      input === "paper" && computerChoice === "rock" ||
-      input === "stone" && computerChoice === "scissors") {
+  if(userWon === true) {
         myOutputValue = `The computer chose ${computerChoice} ${computerIcon} .<br><br>You chose ${input} ${userIcon}.<br><br>You won! Congrats.<br><br>Now you can type "scissors" "paper" or "stone" to play another round!`;
-     } else if ( // Situation where user loses
-      computerChoice === "scissors" && input === "paper" ||
-      computerChoice === "paper" && input === "rock" ||
-      computerChoice === "stone" && input === "scissors") {
-        console.log("worked");
+      } else if (computerWon === true) { // Situation where user loses
         myOutputValue = `The computer chose ${computerChoice} ${computerIcon} .<br><br>You chose ${input} ${userIcon}.<br><br>You lost! Bummer.<br><br>Now you can type "scissors" "paper" or "stone" to play another round!`;
       } else { // Else is a draw
         myOutputValue = `The computer chose ${computerChoice} ${computerIcon} .<br><br>You chose ${input} ${userIcon}.<br><br>Draw!<br><br>Now you can type "scissors" "paper" or "stone" to play another round!`;

26d74e01ac75f22434b52ee057ff32a5f767ac1a
sRanay <jsong2506@gmail.com>
2022-06-01 19:57:17 +0800

    Modified SPS P1

--- day3/in-class/Scissors Paper Stone P1/script.js
+++ day3/in-class/Scissors Paper Stone P1/script.js
@@ -1,12 +1,22 @@
 // Global Variable
 var reversedMode = false;
 var normalMode = true;
+var userName = false;
+var user = "";
+var numberOfGame = 0;
+var numberOfWin = 0;
 
 // Function checkUserInput
 // Input: User Input
 // Output: Return False if it does not have the correct input
 var checkUserInput = function (userInput) {
-  if (userInput === "scissors" || userInput === "paper" || userInput === "stone" || userInput === "reversed" || userInput === "normal") {
+  if (
+    userInput === "scissors" ||
+    userInput === "paper" ||
+    userInput === "stone" ||
+    userInput === "reversed" ||
+    userInput === "normal"
+  ) {
     return true;
   } else {
     return false;
@@ -14,6 +24,7 @@ var checkUserInput = function (userInput) {
 };
 
 // Function generateSPS
+// Purpose: To use Math.random() to generate a choice for the computer
 // Output: Generate a random choice for the computer (scissor, paper or stone)
 var generateSPS = function () {
   var randomDecimal = Math.random() * 3;
@@ -22,13 +33,14 @@ var generateSPS = function () {
   if (number === 1) {
     return "scissors";
   } else if (number === 2) {
-    return "paper"; 
+    return "paper";
   } else {
     return "stone";
   }
 };
 
 // Function convertToIcon
+// Purpose: To convert the words into icons to be displayed on the output
 // Input: scissors, paper or stone
 // Output: converting it into icons
 var convertToIcon = function (choice) {
@@ -41,34 +53,54 @@ var convertToIcon = function (choice) {
   }
 };
 
-var winOrLose = function (user1,user2) {
-  if(
-    user1 === "scissors" && user2 === "paper" ||
-    user1 === "paper" && user2 === "stone" ||
-    user1 === "stone" && user2 === "scissors") {
-      return true;
-    }
-    return false;
+// Function winOrLose
+// Purpose: It is to check whether user1 win user2 (normal mode) or user2 win user1 (reversed mode)
+// Input: user1's choice and user2's choice
+// Output: return true if user1 win (normal mode) or user2 win (reversed mode)
+var winOrLose = function (user1, user2) {
+  if (
+    (user1 === "scissors" && user2 === "paper") ||
+    (user1 === "paper" && user2 === "stone") ||
+    (user1 === "stone" && user2 === "scissors")
+  ) {
+    return true;
+  }
+  return false;
 };
 
+// Function enterReversedMode
+var enterReversedMode = function () {
+  normalMode = false;
+  reversedMode = true;
+};
+
+// Function enterNormalMode
+var enterNormalMode = function () {
+  normalMode = true;
+  reversedMode = false;
+};
+
+// Main function
 var main = function (input) {
-  var myOutputValue = 'you lose';
+  var myOutputValue = "you lose";
+  if (userName === false) {
+    userName = true;
+    user = input;
+    return "Please enter scissors, paper or stone";
+  }
   // Checking for User input
   if (!checkUserInput(input)) {
-    myOutputValue = "Please enter a valid input (scissors, paper or stone)";
-    return myOutputValue
+    return "Please enter a valid input (scissors, paper or stone)";
   }
+  // Checking whether the user entered reversed mode
   if (input === "reversed") {
-    myOutputValue = "Welcome to Reversed Mode."
-    normalMode = false;
-    reversedMode = true;
-    return myOutputValue;
+    enterReversedMode();
+    return "Welcome to Reversed Mode.";
   }
+  // Checking whether the user entered normal mode
   if (input === "normal") {
-    myOutputValue = "Welcome to Normal Mode."
-    normalMode = true;
-    reversedMode = false;
-    return myOutputValue;
+    enterNormalMode();
+    return "Welcome to Normal Mode.";
   }
   var computerChoice = generateSPS();
   var computerIcon = convertToIcon(computerChoice);
@@ -84,13 +116,17 @@ var main = function (input) {
   console.log("User: ", input);
   console.log("Reversed Mode: ", reversedMode);
   console.log("Normal Mode: ", normalMode);
+  numberOfGame += 1;
   // Situation where user wins
-  if(userWon === true) {
-        myOutputValue = `The computer chose ${computerChoice} ${computerIcon} .<br><br>You chose ${input} ${userIcon}.<br><br>You won! Congrats.<br><br>Now you can type "scissors" "paper" or "stone" to play another round!`;
-      } else if (computerWon === true) { // Situation where user loses
-        myOutputValue = `The computer chose ${computerChoice} ${computerIcon} .<br><br>You chose ${input} ${userIcon}.<br><br>You lost! Bummer.<br><br>Now you can type "scissors" "paper" or "stone" to play another round!`;
-      } else { // Else is a draw
-        myOutputValue = `The computer chose ${computerChoice} ${computerIcon} .<br><br>You chose ${input} ${userIcon}.<br><br>Draw!<br><br>Now you can type "scissors" "paper" or "stone" to play another round!`;
-      }
+  if (userWon === true) {
+    numberOfWin += 1;
+    myOutputValue = `The computer chose ${computerChoice} ${computerIcon} .<br><br>You chose ${input} ${userIcon}.<br><br>You won! Congrats.<br><br>So far ${user}, you have won ${numberOfWin}/${numberOfGame} rounds<br><br>Now you can type "scissors" "paper" or "stone" to play another round!`;
+  } else if (computerWon === true) {
+    // Situation where user loses
+    myOutputValue = `The computer chose ${computerChoice} ${computerIcon} .<br><br>You chose ${input} ${userIcon}.<br><br>You lost! Bummer.<br><br>So far ${user}, you have won ${numberOfWin}/${numberOfGame} rounds<br><br>Now you can type "scissors" "paper" or "stone" to play another round!`;
+  } else {
+    // Else is a draw
+    myOutputValue = `The computer chose ${computerChoice} ${computerIcon} .<br><br>You chose ${input} ${userIcon}.<br><br>Draw!<br><br>So far ${user}, you have won ${numberOfWin}/${numberOfGame} rounds<br><br>Now you can type "scissors" "paper" or "stone" to play another round!`;
+  }
   return myOutputValue;
 };
